f96e0a2036f171d41c18b91ab3e354e51335581d
AI2020cs <63743494+AI2020cs@users.noreply.github.com>
2020-04-29 03:34:19 +0200

    Design the game , without AI

---
+++ RouteFinding/RouteFinding/Form1.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace RouteFinding
+{
+	partial class RouteFinding
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RouteFinding));
+			this.HB = new System.Windows.Forms.PictureBox();
+			this.HG = new System.Windows.Forms.PictureBox();
+			this.HJ_Goal = new System.Windows.Forms.PictureBox();
+			this.HH = new System.Windows.Forms.PictureBox();
+			this.HI = new System.Windows.Forms.PictureBox();
+			this.HF = new System.Windows.Forms.PictureBox();
+			this.HC = new System.Windows.Forms.PictureBox();
+			this.HD = new System.Windows.Forms.PictureBox();
+			this.HE = new System.Windows.Forms.PictureBox();
+			this.HA_Start = new System.Windows.Forms.PictureBox();
+			this.PB = new System.Windows.Forms.Label();
+			this.Start_A = new System.Windows.Forms.Label();
+			this.PE = new System.Windows.Forms.Label();
+			this.PD = new System.Windows.Forms.Label();
+			this.PC = new System.Windows.Forms.Label();
+			this.PG = new System.Windows.Forms.Label();
+			this.PF = new System.Windows.Forms.Label();
+			this.PH = new System.Windows.Forms.Label();
+			this.PI = new System.Windows.Forms.Label();
+			this.Goal_J = new System.Windows.Forms.Label();
+			this.btnAstar = new System.Windows.Forms.Button();
+			this.btnUCS = new System.Windows.Forms.Button();
+			this.btnRestart = new System.Windows.Forms.Button();
+			this.btnCost = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.HB)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HG)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HJ_Goal)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HH)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HI)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HF)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HC)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HE)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.HA_Start)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// HB
+			// 
+			this.HB.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HB.Image = ((System.Drawing.Image)(resources.GetObject("HB.Image")));
+			this.HB.Location = new System.Drawing.Point(12, 156);
+			this.HB.Name = "HB";
+			this.HB.Size = new System.Drawing.Size(40, 38);
+			this.HB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HB.TabIndex = 0;
+			this.HB.TabStop = false;
+			this.HB.Click += new System.EventHandler(this.HB_Click);
+			// 
+			// HG
+			// 
+			this.HG.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HG.Image = ((System.Drawing.Image)(resources.GetObject("HG.Image")));
+			this.HG.Location = new System.Drawing.Point(380, 248);
+			this.HG.Name = "HG";
+			this.HG.Size = new System.Drawing.Size(40, 38);
+			this.HG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HG.TabIndex = 1;
+			this.HG.TabStop = false;
+			this.HG.Click += new System.EventHandler(this.HG_Click);
+			// 
+			// HJ_Goal
+			// 
+			this.HJ_Goal.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HJ_Goal.Image = ((System.Drawing.Image)(resources.GetObject("HJ_Goal.Image")));
+			this.HJ_Goal.Location = new System.Drawing.Point(678, 137);
+			this.HJ_Goal.Name = "HJ_Goal";
+			this.HJ_Goal.Size = new System.Drawing.Size(40, 38);
+			this.HJ_Goal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HJ_Goal.TabIndex = 2;
+			this.HJ_Goal.TabStop = false;
+			this.HJ_Goal.Click += new System.EventHandler(this.HJ_Goal_Click);
+			// 
+			// HH
+			// 
+			this.HH.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HH.Image = ((System.Drawing.Image)(resources.GetObject("HH.Image")));
+			this.HH.Location = new System.Drawing.Point(564, 54);
+			this.HH.Name = "HH";
+			this.HH.Size = new System.Drawing.Size(40, 38);
+			this.HH.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HH.TabIndex = 3;
+			this.HH.TabStop = false;
+			this.HH.Click += new System.EventHandler(this.HH_Click);
+			// 
+			// HI
+			// 
+			this.HI.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HI.Image = ((System.Drawing.Image)(resources.GetObject("HI.Image")));
+			this.HI.Location = new System.Drawing.Point(554, 263);
+			this.HI.Name = "HI";
+			this.HI.Size = new System.Drawing.Size(40, 38);
+			this.HI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HI.TabIndex = 4;
+			this.HI.TabStop = false;
+			this.HI.Click += new System.EventHandler(this.HI_Click);
+			// 
+			// HF
+			// 
+			this.HF.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HF.Image = ((System.Drawing.Image)(resources.GetObject("HF.Image")));
+			this.HF.Location = new System.Drawing.Point(452, 121);
+			this.HF.Name = "HF";
+			this.HF.Size = new System.Drawing.Size(40, 38);
+			this.HF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HF.TabIndex = 5;
+			this.HF.TabStop = false;
+			this.HF.Click += new System.EventHandler(this.HF_Click);
+			// 
+			// HC
+			// 
+			this.HC.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HC.Image = ((System.Drawing.Image)(resources.GetObject("HC.Image")));
+			this.HC.Location = new System.Drawing.Point(309, 27);
+			this.HC.Name = "HC";
+			this.HC.Size = new System.Drawing.Size(40, 38);
+			this.HC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HC.TabIndex = 6;
+			this.HC.TabStop = false;
+			this.HC.Click += new System.EventHandler(this.HC_Click);
+			// 
+			// HD
+			// 
+			this.HD.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HD.Image = ((System.Drawing.Image)(resources.GetObject("HD.Image")));
+			this.HD.Location = new System.Drawing.Point(243, 172);
+			this.HD.Name = "HD";
+			this.HD.Size = new System.Drawing.Size(40, 38);
+			this.HD.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HD.TabIndex = 7;
+			this.HD.TabStop = false;
+			this.HD.Click += new System.EventHandler(this.HD_Click);
+			// 
+			// HE
+			// 
+			this.HE.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HE.Image = ((System.Drawing.Image)(resources.GetObject("HE.Image")));
+			this.HE.Location = new System.Drawing.Point(145, 263);
+			this.HE.Name = "HE";
+			this.HE.Size = new System.Drawing.Size(40, 38);
+			this.HE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HE.TabIndex = 8;
+			this.HE.TabStop = false;
+			this.HE.Click += new System.EventHandler(this.HE_Click);
+			// 
+			// HA_Start
+			// 
+			this.HA_Start.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.HA_Start.Image = ((System.Drawing.Image)(resources.GetObject("HA_Start.Image")));
+			this.HA_Start.Location = new System.Drawing.Point(86, 25);
+			this.HA_Start.Name = "HA_Start";
+			this.HA_Start.Size = new System.Drawing.Size(40, 38);
+			this.HA_Start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.HA_Start.TabIndex = 9;
+			this.HA_Start.TabStop = false;
+			this.HA_Start.Click += new System.EventHandler(this.HA_Start_Click);
+			// 
+			// PB
+			// 
+			this.PB.AutoSize = true;
+			this.PB.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PB.ForeColor = System.Drawing.Color.Navy;
+			this.PB.Location = new System.Drawing.Point(9, 140);
+			this.PB.Name = "PB";
+			this.PB.Size = new System.Drawing.Size(50, 13);
+			this.PB.TabIndex = 10;
+			this.PB.Text = "Point_B";
+			this.PB.Click += new System.EventHandler(this.PB_Click);
+			// 
+			// Start_A
+			// 
+			this.Start_A.AutoSize = true;
+			this.Start_A.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Start_A.ForeColor = System.Drawing.Color.Red;
+			this.Start_A.Location = new System.Drawing.Point(83, 9);
+			this.Start_A.Name = "Start_A";
+			this.Start_A.Size = new System.Drawing.Size(51, 13);
+			this.Start_A.TabIndex = 11;
+			this.Start_A.Text = "A_Start";
+			// 
+			// PE
+			// 
+			this.PE.AutoSize = true;
+			this.PE.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PE.ForeColor = System.Drawing.Color.Navy;
+			this.PE.Location = new System.Drawing.Point(142, 247);
+			this.PE.Name = "PE";
+			this.PE.Size = new System.Drawing.Size(49, 13);
+			this.PE.TabIndex = 12;
+			this.PE.Text = "Point_E";
+			// 
+			// PD
+			// 
+			this.PD.AutoSize = true;
+			this.PD.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PD.ForeColor = System.Drawing.Color.Navy;
+			this.PD.Location = new System.Drawing.Point(240, 156);
+			this.PD.Name = "PD";
+			this.PD.Size = new System.Drawing.Size(51, 13);
+			this.PD.TabIndex = 13;
+			this.PD.Text = "Point_D";
+			// 
+			// PC
+			// 
+			this.PC.AutoSize = true;
+			this.PC.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PC.ForeColor = System.Drawing.Color.Navy;
+			this.PC.Location = new System.Drawing.Point(306, 11);
+			this.PC.Name = "PC";
+			this.PC.Size = new System.Drawing.Size(50, 13);
+			this.PC.TabIndex = 14;
+			this.PC.Text = "Point_C";
+			// 
+			// PG
+			// 
+			this.PG.AutoSize = true;
+			this.PG.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PG.ForeColor = System.Drawing.Color.Navy;
+			this.PG.Location = new System.Drawing.Point(377, 232);
+			this.PG.Name = "PG";
+			this.PG.Size = new System.Drawing.Size(51, 13);
+			this.PG.TabIndex = 15;
+			this.PG.Text = "Point_G";
+			// 
+			// PF
+			// 
+			this.PF.AutoSize = true;
+			this.PF.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PF.ForeColor = System.Drawing.Color.Navy;
+			this.PF.Location = new System.Drawing.Point(449, 104);
+			this.PF.Name = "PF";
+			this.PF.Size = new System.Drawing.Size(49, 13);
+			this.PF.TabIndex = 16;
+			this.PF.Text = "Point_F";
+			// 
+			// PH
+			// 
+			this.PH.AutoSize = true;
+			this.PH.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PH.ForeColor = System.Drawing.Color.Navy;
+			this.PH.Location = new System.Drawing.Point(561, 38);
+			this.PH.Name = "PH";
+			this.PH.Size = new System.Drawing.Size(51, 13);
+			this.PH.TabIndex = 17;
+			this.PH.Text = "Point_H";
+			// 
+			// PI
+			// 
+			this.PI.AutoSize = true;
+			this.PI.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PI.ForeColor = System.Drawing.Color.Navy;
+			this.PI.Location = new System.Drawing.Point(551, 247);
+			this.PI.Name = "PI";
+			this.PI.Size = new System.Drawing.Size(48, 13);
+			this.PI.TabIndex = 18;
+			this.PI.Text = "Point_I";
+			// 
+			// Goal_J
+			// 
+			this.Goal_J.AutoSize = true;
+			this.Goal_J.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Goal_J.ForeColor = System.Drawing.Color.Red;
+			this.Goal_J.Location = new System.Drawing.Point(675, 121);
+			this.Goal_J.Name = "Goal_J";
+			this.Goal_J.Size = new System.Drawing.Size(45, 13);
+			this.Goal_J.TabIndex = 19;
+			this.Goal_J.Text = "J_Goal";
+			// 
+			// btnAstar
+			// 
+			this.btnAstar.BackColor = System.Drawing.Color.Navy;
+			this.btnAstar.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnAstar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnAstar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAstar.ForeColor = System.Drawing.Color.White;
+			this.btnAstar.Location = new System.Drawing.Point(22, 377);
+			this.btnAstar.Name = "btnAstar";
+			this.btnAstar.Size = new System.Drawing.Size(120, 48);
+			this.btnAstar.TabIndex = 20;
+			this.btnAstar.Text = "solve by A*";
+			this.btnAstar.UseVisualStyleBackColor = false;
+			// 
+			// btnUCS
+			// 
+			this.btnUCS.BackColor = System.Drawing.Color.Navy;
+			this.btnUCS.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnUCS.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnUCS.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnUCS.ForeColor = System.Drawing.Color.White;
+			this.btnUCS.Location = new System.Drawing.Point(212, 377);
+			this.btnUCS.Name = "btnUCS";
+			this.btnUCS.Size = new System.Drawing.Size(120, 48);
+			this.btnUCS.TabIndex = 21;
+			this.btnUCS.Text = "solve by ucs";
+			this.btnUCS.UseVisualStyleBackColor = false;
+			// 
+			// btnRestart
+			// 
+			this.btnRestart.BackColor = System.Drawing.Color.Purple;
+			this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnRestart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnRestart.ForeColor = System.Drawing.Color.White;
+			this.btnRestart.Location = new System.Drawing.Point(400, 377);
+			this.btnRestart.Name = "btnRestart";
+			this.btnRestart.Size = new System.Drawing.Size(120, 48);
+			this.btnRestart.TabIndex = 22;
+			this.btnRestart.Text = "Restart";
+			this.btnRestart.UseVisualStyleBackColor = false;
+			this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+			// 
+			// btnCost
+			// 
+			this.btnCost.BackColor = System.Drawing.Color.Navy;
+			this.btnCost.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnCost.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnCost.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCost.ForeColor = System.Drawing.Color.White;
+			this.btnCost.Location = new System.Drawing.Point(586, 377);
+			this.btnCost.Name = "btnCost";
+			this.btnCost.Size = new System.Drawing.Size(120, 48);
+			this.btnCost.TabIndex = 23;
+			this.btnCost.Text = "cost between points";
+			this.btnCost.UseVisualStyleBackColor = false;
+			this.btnCost.Click += new System.EventHandler(this.btnCost_Click);
+			// 
+			// RouteFinding
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.ClientSize = new System.Drawing.Size(733, 450);
+			this.Controls.Add(this.btnCost);
+			this.Controls.Add(this.btnRestart);
+			this.Controls.Add(this.btnUCS);
+			this.Controls.Add(this.btnAstar);
+			this.Controls.Add(this.Goal_J);
+			this.Controls.Add(this.PI);
+			this.Controls.Add(this.PH);
+			this.Controls.Add(this.PF);
+			this.Controls.Add(this.PG);
+			this.Controls.Add(this.PC);
+			this.Controls.Add(this.PD);
+			this.Controls.Add(this.PE);
+			this.Controls.Add(this.Start_A);
+			this.Controls.Add(this.PB);
+			this.Controls.Add(this.HA_Start);
+			this.Controls.Add(this.HE);
+			this.Controls.Add(this.HD);
+			this.Controls.Add(this.HC);
+			this.Controls.Add(this.HF);
+			this.Controls.Add(this.HI);
+			this.Controls.Add(this.HH);
+			this.Controls.Add(this.HJ_Goal);
+			this.Controls.Add(this.HG);
+			this.Controls.Add(this.HB);
+			this.Name = "RouteFinding";
+			this.Text = "RouteFinding";
+			this.TopMost = true;
+			((System.ComponentModel.ISupportInitialize)(this.HB)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HG)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HJ_Goal)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HH)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HI)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HF)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HC)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HE)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.HA_Start)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox HB;
+		private System.Windows.Forms.PictureBox HG;
+		private System.Windows.Forms.PictureBox HJ_Goal;
+		private System.Windows.Forms.PictureBox HH;
+		private System.Windows.Forms.PictureBox HI;
+		private System.Windows.Forms.PictureBox HF;
+		private System.Windows.Forms.PictureBox HC;
+		private System.Windows.Forms.PictureBox HD;
+		private System.Windows.Forms.PictureBox HE;
+		private System.Windows.Forms.PictureBox HA_Start;
+		private System.Windows.Forms.Label PB;
+		private System.Windows.Forms.Label Start_A;
+		private System.Windows.Forms.Label PE;
+		private System.Windows.Forms.Label PD;
+		private System.Windows.Forms.Label PC;
+		private System.Windows.Forms.Label PG;
+		private System.Windows.Forms.Label PF;
+		private System.Windows.Forms.Label PH;
+		private System.Windows.Forms.Label PI;
+		private System.Windows.Forms.Label Goal_J;
+		private System.Windows.Forms.Button btnAstar;
+		private System.Windows.Forms.Button btnUCS;
+		private System.Windows.Forms.Button btnRestart;
+		private System.Windows.Forms.Button btnCost;
+	}
+}
+
